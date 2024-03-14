6d98783c4d0ea0be026ce96739b5a74250f30256
Admin <reanajb@gmail.com>
2024-03-14 10:54:11 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const identity = function () {
+
+};
+
+const first = function () {
+  
+};
+  
+const last = function () {
+  
+};
+
+const each = function () {
+  
+};
+
+const indexOf = function () {
+  
+};
+
+const map = function () {
+
+}
+
+const filter = (collection, callback)=>{
+
+}
+
+const reject = (collection, callback)=>{
+
+}
+
+const uniq = (collection) =>{
+
+}
+
+const reduce = (collection, callback, initialVal) =>{
+
+}
+
+module.exports = {
+  identity,
+  first,
+  last,
+  each,
+  indexOf,
+  map,
+  filter, 
+  reject, 
+  uniq, 
+  reduce
+};
